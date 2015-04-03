90a788af7a32638597d73f11ed95aefe162f1efe
David Olesch <david@jackrabbitmobile.com>
2015-04-03 11:23:22 -0500

    Delete keys.js

--- keys.js
+++
@@ -1,3 +0,0 @@
-exports.applicationID = "<!--REPLACE-WITH-YOUR-APPLICATION-ID-->";
-exports.javaScriptKey = "<!--REPLACE-WITH-YOUR-JAVASCRIPT-KEY-->";
-exports.masterKey = "<!--REPLACE-WITH-YOUR-MASTER-KEY-->";
